8e42544ee884c2aa7eea2bc28b38c4b0351ccee9
juanPalalacios08 <juandiegopalacios08mmm@gmail.com>
2024-09-28 21:08:09 -0500

    buscar publicación por Id
    realice el buscar la publicación por id

--- src/main/java/com/example/Proyecto/Is1/Service/IPublicationsService.java
+++ src/main/java/com/example/Proyecto/Is1/Service/IPublicationsService.java
@@ -1,11 +1,12 @@
 package com.example.Proyecto.Is1.Service;
 
 import java.util.List;
-
+import java.util.Optional;
 
 import com.example.Proyecto.Is1.Model.PublicationsModel;
 
 public interface IPublicationsService {
     List<PublicationsModel> getAllPublications ();
     List<PublicationsModel> findAllByTitle(String title);
+    Optional <PublicationsModel> findPublicationsByid(String id);
 }
